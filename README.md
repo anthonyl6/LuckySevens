4f824b61f9bad645ee3e8250f53d78df1ff230f8
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2018-09-20 15:05:50 -0600

    Finished the lucky sevens project.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # LuckySevens
+
+### Name
+Anthony Loria
+
+### ID
+123277
+
+### Date Completed
+September 20, 2018
--- luckysevens/Program.cs
+++ luckysevens/Program.cs
@@ -4,9 +4,26 @@ namespace luckysevens
 {
     class Program
     {
-        static void Main(string[] args)
+        static void  Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totalNumberOfSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for(int i = 1; i <= 100; i++) {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if(die1 + die2 == 7) {
+                    totalNumberOfSevens++;
+                }
+
+            }
+            
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens", totalNumberOfSevens);
+            Console.ReadLine();
         }
     }
 }
